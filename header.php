b79ac9c797972b351454cddbb787c1580845cdb8
TrevorM <tmwagner66@gmail.com>
2025-12-30 11:12:18 -0500

    adding favicon

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<title><?php echo site_global_description(); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/media/favicon.ico">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.png">
 	<?php wp_head(); ?>
 </head>
 
